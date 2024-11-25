aca2979acf954921e9d67f41af5056bc21d09f27
Pavel <workmepaul@gmail.com>
2024-11-25 12:53:37 +0300

    Add basic file structure, add tilwindcss

---
+++ client/src/app/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const HomeScreen = () => {
+	return (
+		<View className='mt-20'>
+			<Text className='text-red-500'>index</Text>
+		</View>
+	)
+}
+
+export default HomeScreen
